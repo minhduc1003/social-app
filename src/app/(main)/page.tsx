562a692cd6cd15493923240d4f211df7562b38f7
minhduc1003 <vuminhduc231003@gmail.com>
2023-09-04 00:23:41 +0700

    add feed in ui

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,14 +1,20 @@
 import FriendList from "./components/FriendList";
 import SideBarMenu from "./components/SideBarMenu";
 import AddNewFeed from "./components/homePage/AddNewFeed";
+import Feed from "./components/homePage/Feed";
 import WrapMainLayout from "./components/homePage/WrapMainLayout";
 export default function Home() {
   return (
     <WrapMainLayout>
       <SideBarMenu />
       {/* <p style={{ height: "1000px" }}>ducdzvcc</p> */}
-      <section>
+      <section style={{ borderRadius: "41px", backgroundColor: "#f9fafb" }}>
         <AddNewFeed></AddNewFeed>
+        {Array(3)
+          .fill(0)
+          .map((i, index) => (
+            <Feed key={index}></Feed>
+          ))}
       </section>
       <FriendList />
     </WrapMainLayout>
