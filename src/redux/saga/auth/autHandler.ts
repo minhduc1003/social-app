534b6e5998ac71ebf886b2f14438dc08a68517bc
minh duc <vuminhduc231003@gmail.com>
2024-04-24 15:18:05 +0700

    fix error when deploy

--- src/redux/saga/auth/autHandler.ts
+++ src/redux/saga/auth/autHandler.ts
@@ -3,10 +3,12 @@ import { getUser } from "./authApi";
 
 import { updateUser } from "@/redux/feature/authSlice";
 import { user } from "@/app/(Auth)/types/type";
-
+type data = {
+  data: user;
+};
 export default function* handleGetUser() {
   try {
-    const res: user = yield call(getUser);
+    const res: data = yield call(getUser);
     yield put(updateUser(res.data));
   } catch (error) {
     console.log(error);
